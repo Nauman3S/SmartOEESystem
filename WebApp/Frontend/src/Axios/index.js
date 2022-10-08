64f96cb0c6b0156965ab8886aa56fa964689d65d
RehanShakir <rehanshakir2@gmail.com>
2022-10-08 13:24:50 +0500

    Frontend Build Added

--- WebApp/Frontend/src/Axios/index.js
+++ WebApp/Frontend/src/Axios/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3500/api",
-  // baseURL: "https://smart-devices-system-backend.caprover.meetin.co.in/api",
+  // baseURL: "http://localhost:3500/api",
+  baseURL: "https://smartoeesystem.production.rehanshakir.com/api",
 
   //   baseURL: process.env.REACT_APP_API,
 });
