19b536b00588db37a608e563f60dccd31acad633
RehanShakir <rehanshakir2@gmail.com>
2023-06-04 19:40:53 +0500

    fix

--- WebApp/Frontend/src/Axios/index.js
+++ WebApp/Frontend/src/Axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:3500/api",
-  baseURL: "https://smartoeesystem.production.rehanshakir.com/api",
+  baseURL: "https://smartoee-system.production.factorytracer.com/api",
 
   //   baseURL: process.env.REACT_APP_API,
 });
